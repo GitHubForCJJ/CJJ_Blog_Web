bff6902ffdefb0f50ebb17e9b5e7b60bfd8e1045
chenjianjun <chenjianjun@DZXZ-JS02983>
2019-07-02 18:27:26 +0800

    1

--- CJJ.Blog.Main/Controllers/CommonController.cs
+++ CJJ.Blog.Main/Controllers/CommonController.cs
@@ -18,29 +18,70 @@ namespace CJJ.Blog.Main.Controllers
         {
             return View();
         }
+        [HttpGet]
+        public ActionResult SysLogin()
+        {
+            return View();
+        }
         /// <summary>
         /// 管理员登录
         /// </summary>
         /// <param name="model"></param>
         /// <returns></returns>
         [HttpPost]
-        public JsonResponse Login(LoginView model)
+        public JsonResponse SysUserLogin(LoginView model)
         {
-            if (model == null)
+            if (model == null||string.IsNullOrWhiteSpace(model.UserAccount)||string.IsNullOrWhiteSpace(model.Password))
             {
-                return new JsonResponse("", 1);
+                return new JsonResponse("", 1,"数据不合法");
             }
-            var emp = BlogHelper.GetModelByWhere_Employee(new Dictionary<string, object>()
+            var srcip = System.Web.HttpContext.Current?.Request?.UserHostAddress;
+            var agent = System.Web.HttpContext.Current?.Request?.UserAgent;
+            var dns = System.Web.HttpContext.Current?.Request?.UserHostName;
+            var res = BlogHelper.EmployeePasswordLogin(model.UserAccount, model.Password, srcip, agent, dns);
+            if (res.IsSucceed)
             {
-                {nameof(Employee.UserAcount),model.UserAccount},
-                {nameof(Employee.IsDeleted),0},
-                {nameof(Employee.States),0}
-            });
-            if (emp == null || emp.KID <= 0 || emp.UserPassword != model.Password)
-            {
-                return new JsonResponse("", 1, "密码错误或者账户不存在");
+                var cookie = new HttpCookie("Token", res.Token);
+                cookie.Domain = ".cjj81.cn";
+                cookie.HttpOnly = true;
+                
+                System.Web.HttpContext.Current?.Response.Cookies.Add(cookie);
+                return new JsonResponse(res, 0, "登录成功");
             }
+            return new JsonResponse("", 1, "登录失败");
 
         }
+        /// <summary>
+        /// 获取客户端IP地址
+        /// </summary>
+        /// <returns>System.String.</returns>
+        public static string GetIP()
+        {
+            try
+            {
+                string result = System.Web.HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+                if (string.IsNullOrEmpty(result))
+                {
+                    result = System.Web.HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+                }
+
+                if (string.IsNullOrEmpty(result))
+                {
+                    result = System.Web.HttpContext.Current.Request.UserHostAddress;
+                }
+
+                if (string.IsNullOrEmpty(result))
+                {
+                    return "0.0.0.0";
+                }
+
+                return result;
+            }
+            catch (Exception)
+            {
+                return "0.0.0.0";
+            }
+        }
+
     }
 }
--- CJJ.Blog.Main/Models/UserData.cs
+++ CJJ.Blog.Main/Models/UserData.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using CJJ.Blog.NetWork.WcfHelper;
+using CJJ.Blog.Service.Model.View;
+using CJJ.Blog.Service.Models.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace CJJ.Blog.Main.Models
 {
-    public static class UserData
+    public class UserData
     {
-
+        public static SysLoginUser  GetSysLoginUser(string token)
+        {
+            if (string.IsNullOrEmpty(token))
+            {
+                return null;
+            }
+            var sys = BlogHelper.GetSysLoginUserByToken(token);
+            return sys;
+        }
     }
 }
