f626ec94e38f88e05ea93ddcdbd654baa3259977
chenjianjun <chenjianjun@DZXZ-JS02983>
2019-07-22 19:00:38 +0800

    1

---
+++ CJJ.Blog.Web/Filters/AccessRecordFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CJJ.Blog.Web.Filters
+{
+    public class AccessRecordFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+
+        }
+    }
+}
