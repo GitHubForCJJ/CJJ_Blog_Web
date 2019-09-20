47284ae20bb403d60d7f387b404ccbaf38849a29
chenjianjun <chenjianjun@DZXZ-JS02983>
2019-09-20 17:52:31 +0800

    1

--- CJJ.Blog.Main/Controllers/CommonController.cs
+++ CJJ.Blog.Main/Controllers/CommonController.cs
@@ -11,6 +11,10 @@ using System.Web.UI.WebControls;
 using LoginView = CJJ.Blog.Main.Models.LoginView;
 using CJJ.Blog.Main.Tools;
 using FastDev.Common.Code;
+using QRCoder;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace CJJ.Blog.Main.Controllers
 {
@@ -29,7 +33,7 @@ namespace CJJ.Blog.Main.Controllers
         public ActionResult SysLogin()
         {
             //WebUtil.WriteCookie(WebUtil.Userinfokey,"",DateTime.Now.AddDays(-5));
-            WebUtil.WriteCookie(WebUtil.Tokenkey,"", DateTime.Now.AddDays(-5));
+            WebUtil.WriteCookie(WebUtil.Tokenkey, "", DateTime.Now.AddDays(-5));
             return View();
         }
         /// <summary>
@@ -42,7 +46,7 @@ namespace CJJ.Blog.Main.Controllers
         {
             if (model == null || string.IsNullOrWhiteSpace(model.UserAccount) || string.IsNullOrWhiteSpace(model.Password))
             {
-                return MyJson(new JsonResponse ("", 1, "数据不合法" ));
+                return MyJson(new JsonResponse("", 1, "数据不合法"));
             }
             var srcip = System.Web.HttpContext.Current?.Request?.UserHostAddress;
             var agent = System.Web.HttpContext.Current?.Request?.UserAgent;
@@ -52,7 +56,7 @@ namespace CJJ.Blog.Main.Controllers
             {
 
                 //WebUtil.WriteCookie(WebUtil.Userinfokey, res.SerializeObject(), DateTime.Parse(res.TokenExpiration));
-                WebUtil.WriteCookie(WebUtil.Tokenkey,res.Token, DateTime.Parse(res.TokenExpiration));              
+                WebUtil.WriteCookie(WebUtil.Tokenkey, res.Token, DateTime.Parse(res.TokenExpiration));
                 return MyJson(new JsonResponse(res, 0, "登录成功"));
             }
             return MyJson(new JsonResponse("", 1, "登录失败"));
@@ -82,7 +86,7 @@ namespace CJJ.Blog.Main.Controllers
         [HttpPost]
         public JsonResult ChangePsw(string psw)
         {
-            var res = new Result() { IsSucceed=false};
+            var res = new Result() { IsSucceed = false };
             if (string.IsNullOrEmpty(psw))
             {
                 return MyJson(res);
@@ -95,6 +99,34 @@ namespace CJJ.Blog.Main.Controllers
                 {nameof(Employee.KID), emp?.KID}
             }, new Service.Models.View.OpertionUser());
             return MyJson(res);
+        }
+
+        /// <summary>
+        /// 创建二维码
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult CreateCode(string key)
+        {
+            // 生成二维码的内容
+            string strCode = string.Format("{0}{1}{2}", @"https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx8e37ff182337cd1f&redirect_uri=http%3a%2f%2frodman.nat300.top%2fapi%2fwchart%2fCodeLogin&response_type=code&scope=snsapi_userinfo&state=", key, @"&connect_redirect=1#wechat_redirect");
+            QRCodeGenerator qrGenerator = new QRCoder.QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(strCode, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrcode = new QRCode(qrCodeData);
+
+            // qrcode.GetGraphic 方法可参考最下发“补充说明”
+            Bitmap qrCodeImage = qrcode.GetGraphic(5, Color.Black, Color.White, null, 15, 6, false);
+            MemoryStream ms = new MemoryStream();
+            qrCodeImage.Save(ms, ImageFormat.Jpeg);
+
+            // 如果想保存图片 可使用  qrCodeImage.Save(filePath);
+
+            //// 响应类型
+            //context.Response.ContentType = "image /Jpeg";
+            ////输出字符流
+            //context.Response.BinaryWrite(ms.ToArray());
+            return File(ms.ToArray(), "image/Jpeg");
         }
 
 
